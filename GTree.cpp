eb9386dda36e499a3eaa1f0de1fb8b4049082b41
Gregg <greggory.png@gmail.com>
2020-03-16 21:30:07 -0700

    9:29 PM 3/16/2020
    Set up the UI and libraries to work with my new "BattlePack" methods, and made my code a lot more readable. Does not binary sort yet, it still heap sorts, but it does so without any complier errors as of now.

--- GTree.cpp
+++ GTree.cpp
@@ -5,18 +5,18 @@
 //Constructor
 GTree::GTree(int LENGTH) {
 	this->LENGTH = LENGTH;
-	heap = new int[LENGTH];
+	tree = new int[LENGTH];
 	
 	//Set all initial values to be 0, which is my version of null for this project because real numbers can only be from 1 to 1000
 	for (int i = 0; i < LENGTH; i++) {
-		heap[i] = 0;
+		tree[i] = 0;
 	}
 	
 	highest = 0;
 }
 
 GTree::~GTree() {
-	delete(heap);
+	delete(tree);
 }
 
 int GTree::getHighest() {
@@ -25,27 +25,27 @@ int GTree::getHighest() {
 
 int GTree::get(int address) {
 	if (address < LENGTH) {
-		return heap[address];
+		return tree[address];
 	}
 	return -1; //Return -1 if the program tries to access an address which isn't allocated in memory
 }
 
 int GTree::getChild1(int address) {
 	if ((address + 1) * 2 - 1 < LENGTH) {
-		return heap[(address + 1) * 2 - 1];
+		return tree[(address + 1) * 2 - 1];
 	}
 	return -1; //Return -1 if the program tries to access an address which isn't allocated in memory
 }
 		
 int GTree::getChild2(int address) {
 	if ((address + 1) * 2 < LENGTH) {
-		return heap[(address + 1) * 2];
+		return tree[(address + 1) * 2];
 	}
 	return -1; //Return -1 if the program tries to access an address which isn't allocated in memory
 }
 
 bool GTree::isEmpty(int address) {
-	if (heap[address] == 0) {
+	if (tree[address] == 0) {
 		return true;
 	}
 	else {
@@ -56,7 +56,7 @@ bool GTree::isEmpty(int address) {
 void GTree::set(int address, int value) {
 	if (address < LENGTH) {
 		//Set that value
-		heap[address] = value;
+		tree[address] = value;
 		
 		//Update highest if necesary
 		if (address > highest) {
@@ -67,7 +67,7 @@ void GTree::set(int address, int value) {
 
 void GTree::setChild1(int address, int value) {
 	if ((address + 1) * 2 - 1 < LENGTH) {
-		heap[(address + 1) * 2 - 1] = value;
+		tree[(address + 1) * 2 - 1] = value;
 		
 		if ((address + 1) * 2 - 1 > highest) {
 			highest = (address + 1) * 2 - 1;
@@ -77,7 +77,7 @@ void GTree::setChild1(int address, int value) {
 
 void GTree::setChild2(int address, int value) {
 	if ((address + 1) * 2 < LENGTH) {
-		heap[(address + 1) * 2] = value;
+		tree[(address + 1) * 2] = value;
 		
 		if ((address + 1) * 2 > highest) {
 			highest = (address + 1) * 2;
